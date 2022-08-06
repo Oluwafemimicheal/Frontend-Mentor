51e0d8f80c4ce87b1c46df65eabac623d17fb1f9
oluwafemimicheal <oluwafemicu@gmail.com>
2022-08-06 19:14:30 +0100

    main

--- static-job-listings-master/index.js
+++ static-job-listings-master/index.js
@@ -6,36 +6,36 @@ window.onload = () => {
         .then(data => {
             let display = data.map((contact) => {
                 return `
-        <ul class="list">
-            <li>
-                <div class="profile">
-                    <img src="${contact.logo}" alt="Loading">
-                    <div class="text-box">
-                        <div class="btn">
-                            <span class="company">${contact.company}</span>
-                            <span class="new">${contact.new}</span>
-                            <span class="featured">${contact.featured}</span>
-                        </div>
-                        <div class="position">
-                            <h2>${contact.position}</h2>
-                        </div>
-                        <div class="posted">
-                            <span>${contact.postedAt}</span>
-                            <span>.</span>
-                            <span>${contact.contract}</span>
-                            <span>.</span>
-                            <span>${contact.location}</span>
-                        </div>
+    <ul class="list">
+        <li>
+            <div class="profile">
+                <img src="${contact.logo}" alt="Loading">
+                <div class="text-box">
+                    <div class="btn">
+                        <span class="company">${contact.company}</span>
+                        <span class="new">${contact.new}</span>
+                        <span class="featured">${contact.featured}</span>
+                    </div>
+                    <div class="position">
+                        <h2>${contact.position}</h2>
+                    </div>
+                    <div class="posted">
+                        <span>${contact.postedAt}</span>
+                        <span>.</span>
+                        <span>${contact.contract}</span>
+                        <span>.</span>
+                        <span>${contact.location}</span>
                     </div>
                 </div>
-                <div class="role">
-                    <h2>${contact.role}</h2>
-                    <h2>${contact.languages}</h2>
-                    <h2>${contact.tools}</h2>
-                </div>
-            </li>
-        </ul>
-       `
+            </div>
+            <div class="role">
+                <h2>${contact.role}</h2>
+                <h2>${contact.languages}</h2>
+                <h2>${contact.tools}</h2>
+            </div>
+        </li>
+    </ul>
+    `
             });
             display = display.join(' ')
             view.innerHTML = display
